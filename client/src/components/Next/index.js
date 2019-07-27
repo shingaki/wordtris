a9248d66fb8d0311bcbb3c7774a6a8c7c22ceee6
Michael Clemons <mwclemons@gmail.com>
2019-07-27 11:46:31 -0500

    added Next to Play

---
+++ client/src/components/Next/index.js
@@ -0,0 +1,74 @@
+import React, { PureComponent } from 'react'
+import { Animate } from 'react-move'
+import { easeExpOut } from 'd3-ease'
+
+const trackStyles = {
+  borderRadius: 4,
+  backgroundColor: 'gray',
+  position: 'relative',
+  margin: '5px 3px 10px',
+  width: 75,
+  height: 125,
+}
+
+class Next extends PureComponent {
+  render() {
+    return (
+      <div>
+        <div>
+          Next Piece:
+        </div>
+        <div style={trackStyles}>
+
+          <div
+            style={{
+              position: 'relative',
+              width: 25,
+              height: 25,
+              borderRadius: 4,
+              opacity: 0.7,
+              backgroundColor: 'rgb(255, 255, 204)',
+              top: 25,
+              left: 25,
+              // WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
+              // transform: `translate3d(${x}px, ${y}px, 0)`,
+            }}>
+              A
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: 25,
+              height: 25,
+              borderRadius: 4,
+              opacity: 0.7,
+              backgroundColor: 'rgb(255, 255, 204)',
+              top: 25,
+              left: 25,
+              // WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
+              // transform: `translate3d(${x}px, ${y}px, 0)`,
+            }}>
+              B
+          </div>
+          <div
+            style={{
+              position: 'relative',
+              width: 25,
+              height: 25,
+              borderRadius: 4,
+              opacity: 0.7,
+              backgroundColor: 'rgb(255, 255, 204)',
+              top: 25,
+              left: 25,
+              // WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
+              // transform: `translate3d(${x}px, ${y}px, 0)`,
+            }}>
+              C
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Next;
