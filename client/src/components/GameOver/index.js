b1c02d2da35b1f29f379726729c5e4de4aa42681
Gabriela RB <gabrielaruizblake@gmail.com>
2019-08-01 20:40:16 -0500

    Game Over Screen Edits

--- client/src/components/GameOver/index.js
+++ client/src/components/GameOver/index.js
@@ -7,26 +7,10 @@ class GameOver extends Component {
       <>
         <div className="row text-center mt-5">
           <div className="col-md-12">
-            <h1>Wordtris</h1>
-          </div>
-        </div>
+            <h1>GAME OVER!</h1>
+            <h4>Your Score: {this.props.score}</h4>
+            <h4>Your Level: {this.props.level}</h4>
 
-        <div className="row text-center mt-3 desk">
-          <div className="col-md-9" style={{ minHeight: 75 }}>
-            <div className="row justify-content-center align-items-center">
-              <div className="col-md-10 text-left">
-                <h1>GAME OVER!</h1>
-              </div>
-            </div>
-
-            <div className="row mt-4">
-              <div className="col-md-10 text-left">
-                <h4>Your Score: {this.props.score}</h4>
-                <h4>Your Level: {this.props.level}</h4>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-3 text-center">
             <h3 className="mb-4">Play Again?</h3>
 
             <button
@@ -37,6 +21,8 @@ class GameOver extends Component {
             </button>
           </div>
         </div>
+
+        
       </>
     );
   }
