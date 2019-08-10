3f4ea8687ff1b7319fcc15d097521aad38210081
Julia Pauletti <julia.pauletti@yahoo.com>
2019-08-10 13:19:40 -0500

    update db with player's new best words, add headers to columns on stats page, key for word/letter bonuses, remove auto login button

--- client/src/components/GlobalHighWords/index.js
+++ client/src/components/GlobalHighWords/index.js
@@ -6,6 +6,13 @@ class GlobalHighWords extends Component {
     render() {
         return (
             <div>
+                <div className="row font-weight-bold col-header">
+                    <div className="col-lg-1 col-md-1 score-chart">Rank</div>
+                    <div className="col-lg-4 col-md-4 score-chart">Player</div>
+                    <div className="col-lg-4 col-md-4 score-chart">Word</div>
+                    <div className="col-lg-3 col-md-2 score-chart text-right">Points</div>
+
+                </div>
                 {this.props.globalHighWords.map((item, i) => {
                     const rowColor = i % 2 === 0 ? 'row even-row' : 'row odd-row'
                     return (
@@ -13,8 +20,7 @@ class GlobalHighWords extends Component {
                             <div className="col-lg-1 col-md-1 score-chart">{item.scorePosition}.</div>
                             <div className="col-lg-4 col-md-4 score-chart">{item.Player.playerName}</div>
                             <div className="col-lg-4 col-md-4 score-chart">{item.highestWord}</div>
-                            <div className="col-lg-2 col-md-2 score-chart text-right">{item.score}</div>
-
+                            <div className="col-lg-3 col-md-2 score-chart text-right">{item.score} {item.wordBonus ? <sup><small>W</small></sup> : ""}{item.letterBonus ? <sup><small>L</small></sup> : ""}</div>
                         </div>)
                 })} </div>)
 
--- client/src/components/PlayersWordsAndScores/index.js
+++ client/src/components/PlayersWordsAndScores/index.js
@@ -7,13 +7,18 @@ class PlayersWordsAndScores extends Component {
         // console.log(this.state.playerStats.playerWordRanking)
         return (
             <div>
+                <div className="row font-weight-bold col-header">
+                    <div className="col-lg-1 col-md-1 score-chart">Rank</div>
+                    <div className="col-lg-6 col-md-6 score-chart">Word</div>
+                    <div className="col-lg-5 col-md-4 text-right score-chart">Points</div>
+                </div>
                 {this.props.playerWordsAndScores.map((item, i) => {
                     let rowColor = i % 2 === 0 ? 'row even-row' : 'row odd-row'
                     return (
                         <div className={rowColor}>
                             <div className="col-lg-1 col-md-1 score-chart">{item.playerWordRanking}.</div>
                             <div className="col-lg-6 col-md-6 score-chart">{item.playerWord}</div>
-                            <div className="col-lg-5 col-md-4 text-right score-chart">Score:  {item.wordPoints}</div>
+                            <div className="col-lg-5 col-md-4 text-right score-chart">{item.wordPoints} {item.wordBonus ? <sup><small>W</small></sup> : ""}{item.letterBonus ? <sup><small>L</small></sup> : ""}</div>
                         </div>)
                 })} </div>)
 
