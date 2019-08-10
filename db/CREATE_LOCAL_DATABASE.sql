505184ed66bef4e9372c71a5dcc7b23a1b2d6938
Tamami <tamami_polk@hotmail.com>
2019-08-10 11:43:09 -0500

    added letterBonus and wordBonus to Words tables

--- db/CREATE_LOCAL_DATABASE.sql
+++ db/CREATE_LOCAL_DATABASE.sql
@@ -20,11 +20,11 @@ INSERT INTO highestscores (scorePosition, highestScore, createdAt, updatedAt, Pl
 INSERT INTO highestscores (scorePosition, highestScore, createdAt, updatedAt, PlayerId) VALUES (5, 12, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 3);
 
 # SEED RECORDS FOR HIGHEST WORDS
-INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdAt, updatedAt, PlayerId) VALUES (1, 'highway', 20, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
-INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdAt, updatedAt, PlayerId) VALUES (2, 'parkway', 19, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
-INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdAt, updatedAt, PlayerId) VALUES (3, 'roadway', 14, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
-INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdAt, updatedAt, PlayerId) VALUES (4, 'today', 13, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 2);
-INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdAt, updatedAt, PlayerId) VALUES (5, 'country', 12, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 3);
+INSERT INTO highestwords (scorePosition, highestWord, score, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES (1, 'highway', 20, 0, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
+INSERT INTO highestwords (scorePosition, highestWord, score, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES (2, 'parkway', 19, 0, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
+INSERT INTO highestwords (scorePosition, highestWord, score, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES (3, 'roadway', 14, 0, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 4);
+INSERT INTO highestwords (scorePosition, highestWord, score, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES (4, 'today', 13, 0, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 2);
+INSERT INTO highestwords (scorePosition, highestWord, score, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES (5, 'country', 12, 0, 0, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 3);
 
 
 # PLAYER WORDS : TOP 5 WORDS (WORDS WITH THE HIGHEST SCORES) RECEIVED BY THE PLAYER SINCE THE PLAYER BEGAN PLAYING WORDTRIS
@@ -34,33 +34,33 @@ INSERT INTO highestwords (scorePosition, highestWord, score, bonusUsed, createdA
 
 
 # PLAYER ONE HAS '5' SEED RECORDS FOR PLAYER WORDS AND PLAYER SCORES
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('knowledgeable', 24, 1, 0, current_date, current_date, 1);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('keyboard', 18, 4, 0, current_date, current_date, 1);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('jubilantly', 22, 2, 0, current_date, current_date, 1);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('kaleidoscope', 21, 3, 0, current_date, current_date, 1);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('abandoned', 13, 2, 0, current_date, current_date, 1);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('knowledgeable', 24, 1, 0, 0, current_date, current_date, 1);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('keyboard', 18, 4, 0, 0, current_date, current_date, 1);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('jubilantly', 22, 2, 0, 0, current_date, current_date, 1);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('kaleidoscope', 21, 3, 0, 0, current_date, current_date, 1);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('abandoned', 13, 2, 0, 0, current_date, current_date, 1);
 
 # PLAYER TWO HAS '4' SEED RECORDS FOR PLAYER WORDS
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('tomorrow', 9, 4, 0, current_date, current_date, 2);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('today', 13, 3, 0, current_date, current_date, 2);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('knockout', 18, 1, 0, current_date, current_date, 2);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('ladybug', 14, 2, 0, current_date, current_date, 2);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('tomorrow', 9, 4, 0, 0, current_date, current_date, 2);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('today', 13, 3, 0, 0, current_date, current_date, 2);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('knockout', 18, 1, 0, 0, current_date, current_date, 2);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('ladybug', 14, 2, 0, 0, current_date, current_date, 2);
 
 # PLAYER THREE HAS '4' SEED RECORDS FOR PLAYER WORDS
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('hamburger', 17, 4, 0, current_date, current_date, 3);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('bridge', 10, 1, 0, current_date, current_date, 3);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('staunchest', 15, 3, 0, current_date, current_date, 3);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('picture', 11, 2, 0, current_date, current_date, 3);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('hamburger', 17, 4, 0, 0, current_date, current_date, 3);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('bridge', 10, 1, 0, 0, current_date, current_date, 3);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('staunchest', 15, 3, 0, 0, current_date, current_date, 3);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('picture', 11, 2, 0, 0, current_date, current_date, 3);
 
 # PLAYER FOUR HAS '4' SEED RECORDS FOR PLAYER WORDS
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('avenue', 9, 1, 0, current_date, current_date, 4);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('parkway', 19, 3, 0, current_date, current_date, 4);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('highway', 20, 4, 0, current_date, current_date, 4);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('roadway', 14, 2, 0, current_date, current_date, 4);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('avenue', 9, 1, 0, 0, current_date, current_date, 4);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('parkway', 19, 3, 0, 0, current_date, current_date, 4);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('highway', 20, 4, 0, 0, current_date, current_date, 4);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('roadway', 14, 2, 0, 0, current_date, current_date, 4);
 
 # PLAYER FIVE HAS '2' SEED RECORDS FOR PLAYER WORDS
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('movie', 10, 1, 0, current_date, current_date, 5);
-INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, bonusUsed, createdAt, updatedAt, PlayerId) VALUES ('sitcom', 10, 2, 0, current_date, current_date, 5);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('movie', 10, 1, 0, 0, current_date, current_date, 5);
+INSERT INTO playerwords (playerWord, wordPoints, playerWordRanking, letterBonus, wordBonus, createdAt, updatedAt, PlayerId) VALUES ('sitcom', 10, 2, 0, 0, current_date, current_date, 5);
 
 # PLAYERS SEED INSERT STATEMENTS FOR playerscores
 INSERT INTO playerscores (playerScore, createdAt, updatedAt, PlayerId, playerScoreRanking) VALUES (24, '2019-08-06 00:00:00', '2019-08-06 00:00:00', 1, 4);
